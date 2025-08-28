f1eeb2d432b088e0aed01506908b1522259d9295
Sara Pabón González <sarapabon1123527@correo.itm.edu.co>
2025-08-28 08:51:31 -0500

    Segundo Commit - Creación de clases en Program.cs

--- Sara_Pabon_Proyecto/Program.cs
+++ Sara_Pabon_Proyecto/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Vehiculos{
+
+}//Fin Vehiculos
+
+public class Carros : Vehiculos {
+
+}//Fin Carros
+
+public class Motos : Vehiculos {
+
+}//Fin Motos
+
+public class Propietarioss {
+    
+}//Fin Propietarios
