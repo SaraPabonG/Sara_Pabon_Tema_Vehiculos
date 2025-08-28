c37246c65ac0f093f7fc7ff27f82cbec95b12230
Sara Pabón González <sarapabon1123527@correo.itm.edu.co>
2025-08-28 09:22:03 -0500

    Cuarto commit - Asignando atributos a las clases e instanciando las clases

--- Sara_Pabon_Proyecto/Program.cs
+++ Sara_Pabon_Proyecto/Program.cs
@@ -1,18 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-public class Vehiculos{
+Vehiculos vehiculo_1 = new Vehiculos();
+Vehiculos vehiculo_2 = new Carros();
+Vehiculos vehiculo_3 = new Motos();
 
+public class Vehiculos{
+    public int Id;
+    public string? Nombre = "";
+    public decimal Kilometraje;
+    public bool Usado;
+    public DateTime Fecha;
 }//Fin Vehiculos
 
 public class Carros : Vehiculos {
-
+    public int CantNeumaticos;
+    public List<Propietarios>? Propietarios;
 }//Fin Carros
 
 public class Motos : Vehiculos {
-
+    public string? TipoManubrio;
+    public List<Propietarios>? Propietarios;
 }//Fin Motos
 
-public class Propietarioss {
-    
+public class Propietarios {
+    public int Id;
+    public string? Nombre;
 }//Fin Propietarios
